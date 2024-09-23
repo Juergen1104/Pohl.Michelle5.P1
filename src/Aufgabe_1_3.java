69e7446809146bb37e41a84a132f2a14396cf073
jpohl <jpohl@web.de>
2024-09-23 21:43:51 +0200

    test

--- src/Aufgabe_1_3.java
+++ src/Aufgabe_1_3.java
@@ -1,25 +1,20 @@
 public class Aufgabe_1_3 {
 
     public static void main(String[] args) {
-//        System.out.println("Bitte geben Sie folgende Daten ein:");
-//
-//
-//    }
-
-        // Eingabe des Grundkapitals
+        // Eingabe  kapitals
         double kapital = IOTools.readDouble("Geben Sie Ihr Grundkapital K0 ein: ");
 
-        // Eingabe des Zinssatzes
-        double zinssatz = IOTools.readDouble("Geben Sie den Zinssatz p ein (in Prozent): ");
+        // Eingabe Zinssatzes
+        double zins = IOTools.readDouble("Geben Sie den Zinssatz p ein (in Prozent): ");
 
-        // Eingabe der Anzahl der Jahre
+        // Eingabe Jahre
         int jahre = IOTools.readInteger("Geben Sie die Anzahl der Jahre j ein: ");
 
-        // Berechnung des Kapitalwerts nach j Jahren
-        double kapitalNachJahren = kapital * Math.pow(1 + (zinssatz / 100), jahre);
+        // Berechnung Wert
+        double kapitalNachJahren = kapital * Math.pow(1 + (zins / 100), jahre);
 
         // Ausgabe des Ergebnisses
-        System.out.printf("Nach %d Jahren beträgt Ihr Kapital: %.2f Euro%n", jahre, kapitalNachJahren);
+        System.out.println("Nach " + jahre + " Jahren beträgt Ihr Kapital: " + kapitalNachJahren;
 
     }
 }
--- src/Aufgabe_1_4.java
+++ src/Aufgabe_1_4.java
@@ -1,14 +1,11 @@
-import java.io.*;
-import java.util.*;
-
 public class Aufgabe_1_4 {
 
     public static void main(String[] args) {
-        // Eingabe der ersten Uhrzeit
+        // Start
         int stundenBeginn = IOTools.readInteger("Geben Sie die Stunden des Beginns der Parkzeit ein: ");
         int minutenBeginn = IOTools.readInteger("Geben Sie die Minuten des Beginns der Parkzeit ein: ");
 
-        // Eingabe der zweiten Uhrzeit
+        // Abfahrt
         int stundenEnde = IOTools.readInteger("Geben Sie die Stunden des Endes der Parkzeit ein: ");
         int minutenEnde = IOTools.readInteger("Geben Sie die Minuten des Endes der Parkzeit ein: ");
 
@@ -16,19 +13,20 @@ public class Aufgabe_1_4 {
         int gesamtBeginn = stundenBeginn * 60 + minutenBeginn;
         int gesamtEnde = stundenEnde * 60 + minutenEnde;
 
-        // Berechnung der Parkdauer in Minuten
+        // Parkdauer Minuten
         int parkdauerInMinuten = gesamtEnde - gesamtBeginn;
 
-        // Berechnung der angefangenen Stunden
+
+        // Stunden
         int volleStunden = parkdauerInMinuten / 60;
         int restMinuten = parkdauerInMinuten % 60;
 
-        // Berechnung der Parkgebühr
+        // Kosten
         int angefangeneStunden = (restMinuten > 0) ? volleStunden + 1 : volleStunden;
         double parkgebuehr = angefangeneStunden * 1.50;
 
         // Ausgabe der Ergebnisse
-        System.out.printf("Parkdauer: %d Minuten%n", parkdauerInMinuten);
-        System.out.printf("Parkgebühr: %.2f \u20AC%n", parkgebuehr);
+        System.out.println("Parkdauer: " + parkdauerInMinuten + " Minuten");
+        System.out.println("Parkgebühr: " + parkgebuehr);
     }
 }
